33305a2dc25d23f4540badce020e2fde1cb0987f
Jingke Xu <jinxu@c1r2p6.hive.fi>
2025-09-18 16:16:13 +0300

    signal handling done

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jinxu <jinxu@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 17:06:13 by jinxu             #+#    #+#             */
-/*   Updated: 2025/09/17 17:38:19 by jinxu            ###   ########.fr       */
+/*   Updated: 2025/09/18 16:10:41 by jinxu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,23 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include<signal.h>
+
+void sig_handler(int signum)
+{
+	(void)signum;
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
 
 int main(void)
 {
 	char	*line;
 
+	signal(SIGINT, sig_handler);
+	signal(SIGQUIT,SIG_IGN);
 	while (1)
 	{
 		line = readline("minishell$ ");
