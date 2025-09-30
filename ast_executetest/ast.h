5739f21276230cbc4f64b79e186cca8e662a6f8c
GinkoHsu <ginkohsu@gmail.com>
2025-09-30 14:03:29 +0300

    ast testfiles for making execution part

---
+++ ast_executetest/ast.h
@@ -0,0 +1,62 @@
+/* ************************************************************************* */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinxu <jinxu@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/30 11:15:02 by jinxu             #+#    #+#             */
+/*   Updated: 2025/09/30 13:41:20 by jinxu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef AST_H
+# define AST_H
+
+#include "minishell.h"
+
+typedef enum e_redir_type
+{
+	REDIR_IN,
+	REDIR_OUT,
+	REDIR_APPEND,
+	REDIR_HEREDOC
+}	t_redir_type;
+
+typedef struct s_redir
+{
+	t_redir_type	type;
+	char			*filename;
+	struct s_redir	*next;
+}	t_redir;
+
+typedef struct s_command
+{
+	char		**argv;
+	t_redir		*redirs;
+}	t_command;
+
+typedef enum e_node_type
+{
+	CMD,
+	PIPE
+}	t_node_type;
+
+typedef struct s_ast
+{
+	t_node_type	type;
+	union
+	{
+		t_command	cmd;
+		struct
+		{
+			struct s_ast	*left;
+			struct s_ast	*right;
+		}	pipe;
+	};
+}	t_ast;
+
+t_ast	*create_test_ast(void);
+void	execute_ast(t_ast *ast);//Amit's execute part?
+void	free_ast(t_ast *ast);
+
+#endif
