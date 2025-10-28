f47abbb74a516ce7f4ec2a48527399c47cc86d28
Jingke Xu <jinxu@c3r3p4.hive.fi>
2025-10-28 19:29:02 +0200

    fixed empty commands & redir syntax error detection

--- src/parse_pipeline.c
+++ src/parse_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: jinxu <jinxu@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 15:41:59 by jinxu             #+#    #+#             */
-/*   Updated: 2025/10/26 21:19:29 by jinxu            ###   ########.fr       */
+/*   Updated: 2025/10/28 19:00:51 by jinxu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,41 @@ static t_ast	*handle_pipe_expression(t_parser *parser, t_ast *left)
 	return (create_pipe_node(left, right));
 }
 
+static int	check_redirect_syntax(t_parser *parser)
+{
+	int			i;
+	t_token*	next_token;
+
+	i = 0;
+	while (i < (parser->token_count - 1))
+	{
+		if (is_redirection_token(&parser->tokens[i]))
+		{
+			next_token = &parser->tokens[i + 1];
+			if (next_token->type == TOKEN_EOF)
+			{
+				printf("syntax error near unexpected token 'newline'\n");
+				return (1);
+			}
+			if (!is_string_token(next_token))
+			{
+				printf("syntax error near unexpected token '%s'\n", 
+				next_token->value);
+				return (1);
+			}
+		}
+		i++;
+	}
+	return (0);
+}
+
 t_ast	*parse_pipeline(t_parser *parser)
 {
 	t_ast	*left;
 	t_token	*token;
-	
+
+	if (check_redirect_syntax(parser))
+		return (NULL);
 	left = parse_command(parser);
 	if (!left)
 		return (NULL);
