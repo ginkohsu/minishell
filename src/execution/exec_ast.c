ae9503aaa084c5c32efddfc2959eb5f6e548d6ad
Jingke Xu <jinxu@c2r1p9.hive.fi>
2025-11-07 16:31:05 +0200

    h files reorganized

--- src/execution/exec_ast.c
+++ src/execution/exec_ast.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "execution.h"
+#include "minishell.h"
 
 static t_ast	*g_ast_root = NULL;
 
